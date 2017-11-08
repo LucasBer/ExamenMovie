ce9bf6c1f84cc868427d2121cdd9534b070ef4c0
LucasBer <lucas.berghs@gmail.com>
2017-11-08 16:37:54 +0100

    CheckService implemented
    Zal eerst checken of de naam al in de database zit, zoja zal hij van CouchDB halen, zoniet zoekt hij het op en slaagt hij het ook op.

--- app/_attachments/scripts/app.js
+++ app/_attachments/scripts/app.js
@@ -12,21 +12,30 @@ angular.module('MovieApp', ['ngRoute'])
                 redirectTo: '/home'
             });
     })
-    .controller('homeCtrl', function($scope, getServ, saveServ){
+    .controller('homeCtrl', function($scope, getServ, saveServ, checkServ){
     	$("#buttonID").click(function (event){
-    		getServ.getMovies().then(function (data){
-    			var dataString = data + "";
-    			$scope.titel = dataString;
-    			saveServ.saveActor(data);
-    		})
+    		checkServ.checkActor().then(function (data){
+    			var jsonobj = JSON.parse(data);
+    			if (jsonobj._id == $('#inputID').val()){
+    				$scope.titel = jsonobj.movies + "";
+    				console.log("Checked en in Database.");
+    			}
+    			else {
+    	    		getServ.getMovies().then(function (data){
+    	    			var dataString = data + "";
+    	    			$scope.titel = dataString;
+    	    			saveServ.saveActor(data);
+    	    			console.log("Checked not in database and saved");
+    			})
+    		}
     	})
+    })
     })
     .service('getServ', function($q){
     	var q = $q.defer();
     	this.getMovies = function(){
     		var url = 'http://theimdbapi.org/api/find/person?name=' + $('#inputID').val();
     		$.get(url).then(function (data){
-    			var actorID = data[0].person_id;
     			var dataActor = data[0].filmography.actor
     			var titels = [];
     			for (var j = 0; j < dataActor.length; j++){
@@ -43,3 +52,12 @@ angular.module('MovieApp', ['ngRoute'])
     		$http.put('../../'+$("#inputID").val(), object);
     	}
     })
+    .service("checkServ", function($q){
+    	var q = $q.defer();
+    	this.checkActor = function(){
+    		$.get('../../'+$("#inputID").val()).then(function (data){
+    			q.resolve(data);
+  		}, function error (err){ q.reject(err);});
+    		return q.promise;
+    	}
+    });
