d58c7079e7993c78378234cf0100299afb1602cc
LucasBer <lucas.berghs@gmail.com>
2017-11-08 15:18:51 +0100

    Functionele button/input
    Button toegevoegd die gelinkt is aan een inputveld, die uiteindelijk succesvol een werkende imdb link logt in de console.

--- app/_attachments/scripts/app.js
+++ app/_attachments/scripts/app.js
@@ -12,9 +12,29 @@ angular.module('MovieApp', ['ngRoute'])
                 redirectTo: '/home'
             });
     })
-    .controller('homeCtrl', function($scope){
-      //Code
+    .controller('homeCtrl', function(getServ){
+    	$("#buttonID").click(function (event){
+    		getServ.getActor();
+    	})
     })
     .service('getServ', function(){
-    	//Code
+    	this.getActor = function(){
+    		var url = 'http://theimdbapi.org/api/find/person?name=' + $('#inputID').val();
+    		console.log(url);
+    	}
+    	/*
+    	var q = $q.defer();
+    	var id = "ea5ff900d53cd148d636010dbe00088e";
+    	this.getItem = function() {
+    		$.get(url+id).then(function (data){
+    			var item = JSON.parse(data);
+    			item.naam = $("#inputID").val();
+    			$http.put(url+item._id, JSON.stringify(item)).then(function (data){
+    				q.resolve(JSON.parse(data.config.data));
+    			}, function error (err){ q.reject(err);})
+    		})
+    		return q.promise;
+    	}
+    });
+    */
     });
